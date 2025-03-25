4f284f7e4867035d2bf1fb1bb4242397bc8e8acc
David McCullough <dmccullough488@gmail.com>
2025-03-24 23:13:51 -0500

    Added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react"
 
-export default function Hangman() {
+export default function AssemblyEndgame() {
 
   return (
-    <div className="game-container">
+    <main>
       <header>
         <h1>Assembly: Endgame</h1>
         <p>Guess the word to prove if it's enough to keep the programming world safe from Assembly!</p>
       </header>
 
-      <main>
-        Game goes here
-      </main>
-    </div>
+      Game goes here
+    </main>
   )
 }
 
