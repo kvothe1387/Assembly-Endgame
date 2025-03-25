333931e8f69d5d001ac80b0fa3a0c64104210ecb
David McCullough <dmccullough488@gmail.com>
2025-03-24 23:09:00 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Hangman() {
+
+  return (
+    <div className="game-container">
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word to prove if it's enough to keep the programming world safe from Assembly!</p>
+      </header>
+
+      <main>
+        Game goes here
+      </main>
+    </div>
+  )
+}
+
