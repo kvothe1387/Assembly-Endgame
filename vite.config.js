22450e1de0444b0af3b868d4c18ea374f115ff5a
David McCullough <dmccullough488@gmail.com>
2025-05-06 00:04:12 -0500

    Setup github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/assembly-endgame/', // Replace with your repo name
+  base: '/Assembly-Endgame/', // Replace with your repo name
 })
